80c54f924e5efd466b5e543d2f32af41dde9fa0a
ask-shay <7akshayshelar@example.com>
2023-12-12 10:13:38 +0530

    localhost changed

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import  axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = "https://uniton.onrender.com/api/"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NmMyNjcyMDVlMTc3NmI5NWJhNWNmYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwMTk1NDg4MCwiZXhwIjoxNzAyMjE0MDgwfQ.I_wEVFbHBWtAxroO10cx44hi-_5CvlQ5T5ds2SUvonc"
 
 export const publicRequest = axios.create({
